711baf3276292b21860e85b98364e92716a983d0
XCodeSpace <mylogosxie@gmail.com>
2024-04-17 18:18:44 +0800

    add go sdk interface

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -17,3 +17,91 @@
  */
 
 package sdk
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/xcodespace/go-sdk/erc_6551"
+)
+
+type Erc6551Sdk struct {
+	privateKey           *ecdsa.PrivateKey
+	nft721ContractAddr   common.Address
+	registryContractAddr common.Address
+	accountContractAddr  common.Address
+	address              common.Address
+	rpcClient            *ethclient.Client
+	chainID              *big.Int
+}
+
+func (this *Erc6551Sdk) NewErc6551Sdk(privateKey *ecdsa.PrivateKey, nft721ContractAddr, registryContractAddr,
+	accountContractAddr common.Address, rpcUrl string, chainID *big.Int) (*Erc6551Sdk, error) {
+	rpcClient, err := ethclient.Dial(rpcUrl)
+	if err != nil {
+		return nil, err
+	}
+	return &Erc6551Sdk{
+		privateKey:           privateKey,
+		nft721ContractAddr:   nft721ContractAddr,
+		registryContractAddr: registryContractAddr,
+		accountContractAddr:  accountContractAddr,
+		address:              crypto.PubkeyToAddress(privateKey.PublicKey),
+		rpcClient:            rpcClient,
+		chainID:              chainID,
+	}, nil
+}
+func (this *Erc6551Sdk) Mint721Nft(tokenId *big.Int, uri string) (*types.Transaction, error) {
+	erc721Nft, err := erc_6551.NewERC721Asset(this.nft721ContractAddr, this.rpcClient)
+	if err != nil {
+		return nil, err
+	}
+	transactOpts, err := bind.NewKeyedTransactorWithChainID(this.privateKey, this.chainID)
+	if err != nil {
+		return nil, err
+	}
+	addrNonce, err := this.rpcClient.PendingNonceAt(context.Background(), this.address)
+	if err != nil {
+		return nil, err
+	}
+	transactOpts.From = this.address
+	transactOpts.Nonce = big.NewInt(int64(addrNonce))
+	transactOpts.Value = big.NewInt(0)
+	return erc721Nft.Mint(transactOpts, tokenId, uri)
+}
+
+//computes the token bound account address
+func (this *Erc6551Sdk) Account(tokenId *big.Int) (common.Address, error) {
+	registry, err := erc_6551.NewERC6551Registry(this.registryContractAddr, this.rpcClient)
+	if err != nil {
+		return common.Address{}, err
+	}
+	salt := *new([32]byte)
+	return registry.Account(nil, this.accountContractAddr, salt, this.chainID, this.nft721ContractAddr, tokenId)
+}
+
+func (this *Erc6551Sdk) CreateAccount(tokenId *big.Int) (*types.Transaction, error) {
+	registry, err := erc_6551.NewERC6551Registry(this.registryContractAddr, this.rpcClient)
+	if err != nil {
+		return nil, err
+	}
+	salt := *new([32]byte)
+	transactOpts, err := bind.NewKeyedTransactorWithChainID(this.privateKey, this.chainID)
+	if err != nil {
+		return nil, err
+	}
+	addrNonce, err := this.rpcClient.PendingNonceAt(context.Background(), this.address)
+	if err != nil {
+		return nil, err
+	}
+	transactOpts.From = this.address
+	transactOpts.Nonce = big.NewInt(int64(addrNonce))
+	transactOpts.Value = big.NewInt(0)
+	return registry.CreateAccount(transactOpts, this.accountContractAddr, salt, this.chainID, this.nft721ContractAddr, tokenId)
+}
